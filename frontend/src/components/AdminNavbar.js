24f8a42c83806560f22cef18280329d1a7ee347e
PrideAgroSolutions <prideagrosolution@gmail.com>
2025-01-30 21:46:56 +0530

    Navbar Commit

--- frontend/src/components/AdminNavbar.js
+++ frontend/src/components/AdminNavbar.js
@@ -1,28 +1,25 @@
 import React, { useState } from 'react';
-import logo from '../assets/logo.png'
-
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { authAction } from '../store';
 
 const AdminNavbar = () => {
-
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector((state) => state.isLoggedIn)
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
 
   const handleLogout = () => {
     let confirmLogout = window.confirm("Are you Sure to Logout!");
     if (!confirmLogout) {
       return;
     }
-    dispatch(authAction.logout())
+    dispatch(authAction.logout());
     sessionStorage.removeItem("AdminId");
     sessionStorage.removeItem("role");
     window.location.reload();
-
-  }
-
+  };
 
   const [isMenuOpen, setIsMenuOpen] = useState(true);
 
@@ -34,7 +31,7 @@ const AdminNavbar = () => {
             <img onClick={() => window.location.reload()} className='w-20' src={logo} />
           </div>
           <button
-            className="absolute right-1 mr-4 md:hidden  text-white focus:outline-none"
+            className="absolute right-1 mr-4 md:hidden text-white focus:outline-none"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             <svg
@@ -54,54 +51,53 @@ const AdminNavbar = () => {
           </button>
         </div>
 
-
-        <ul className={`lg:hidden md:hidden absolute top-16 gap-16 px-8 py-2 left-0 w-full bg-gradient-to-r from-yellow-500 via-brown-500 to-green-700 text-white md:static md:flex md:space-x-6 md:w-8  md:w-auto transition-all duration-900 ${isMenuOpen ? 'hidden' : 'block'} md:gap-6`}>
+        <ul className={`lg:hidden md:hidden absolute top-16 gap-16 px-8 py-2 left-0 w-full bg-gradient-to-r from-yellow-500 via-brown-500 to-green-700 text-white md:static md:flex md:space-x-6 md:w-auto transition-all duration-900 ${isMenuOpen ? 'hidden' : 'block'} md:gap-6`}>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-            <a href="/admin">Home</a>
+            <Link to="/admin">Home</Link>
           </li>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-            <a href="/admin/order">Orders</a>
+            <Link to="/admin/order">Orders</Link>
           </li>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-            <a href="/admin/product">Products</a>
+            <Link to="/admin/product">Products</Link>
           </li>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-          <a href="/admin/results/">Result</a>
+            <Link to="/admin/results/">Result</Link>
           </li>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-          <a href="/admin/create">CreateAdmin</a>
+            <Link to="/admin/create">CreateAdmin</Link>
           </li>
         </ul>
 
-
         <div>
-          <ul className={`hidden md:flex gap-16 bg-gradient-to-r from-[#ada417] via-[#cfac1e] to-[#59902c]  px-8 py-2 left-0 w-full text-white`}>
+          <ul className="hidden md:flex gap-16 bg-gradient-to-r from-[#ada417] via-[#cfac1e] to-[#59902c] px-8 py-2 left-0 w-full text-white">
             <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-              <a href="/admin">Home</a>
+              <Link to="/admin">Home</Link>
             </li>
             <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-              <a href="/admin/order">Orders</a>
+              <Link to="/admin/order">Orders</Link>
             </li>
             <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-              <a href="/admin/product">Products</a>
+              <Link to="/admin/product">Products</Link>
             </li>
             <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-              <a href="/admin/results/">Result</a>
+              <Link to="/admin/results/">Result</Link>
             </li>
             <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-              <a href="/admin/create">CreateAdmin</a>
+              <Link to="/admin/create">CreateAdmin</Link>
             </li>
           </ul>
         </div>
 
-
-          {!isLoggedIn &&
-            <button className='p-2 bg-orange-400 mr-10 rounded-md'><a href="/admin/Login" className=''>Login</a></button>
-          }
-          {isLoggedIn &&
-            <button onClick={handleLogout} className='p-2 bg-red-400 mr-10 rounded-md'>Logout</button>
-          }
-        </div>
+        {!isLoggedIn && (
+          <button className='p-2 bg-orange-400 mr-10 rounded-md'>
+            <Link to="/admin/Login">Login</Link>
+          </button>
+        )}
+        {isLoggedIn && (
+          <button onClick={handleLogout} className='p-2 bg-red-400 mr-10 rounded-md'>Logout</button>
+        )}
+      </div>
     </nav>
   );
 };
--- frontend/src/components/UserNavbar.js
+++ frontend/src/components/UserNavbar.js
@@ -1,39 +1,34 @@
 import React, { useState } from 'react';
-import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 import { VscAccount } from "react-icons/vsc";
 import { FaCartShopping } from "react-icons/fa6";
-
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { authAction } from '../store';
 
 const Navbar = () => {
-
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector((state) => state.isLoggedIn)
-  
-  const handleLogout = () => {
-    let confirmLogout = window.confirm("Are you Sure to Logout!");
-    if(!confirmLogout) {
-      return;
-    }
-    dispatch(authAction.logout())
-    sessionStorage.removeItem("Id");
-    
-  }
-
-
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
   const [isMenuOpen, setIsMenuOpen] = useState(true);
 
+  const handleLogout = () => {
+    let confirmLogout = window.confirm("Are you sure you want to logout?");
+    if (!confirmLogout) {
+      return;
+    }
+    dispatch(authAction.logout());
+    sessionStorage.removeItem("Id");
+  };
+
   return (
-    <nav className="select-none sticky top-0 z-10 bg-gradient-to-r from-yellow-500 via-brown-500 to-green-700 py-4 px-6 md:px-8 ">
+    <nav className="select-none sticky top-0 z-10 bg-gradient-to-r from-yellow-500 via-brown-500 to-green-700 py-4 px-6 md:px-8">
       <div className="container flex justify-between items-center">
         <div className='flex items-center'>
           <div className="text-yellow-700 text-2xl font-bold cursor-pointer">
-            <img onClick={() => window.location.reload()} className='w-20' src={logo} />
+            <img onClick={() => window.location.reload()} className='w-20' src={logo} alt="Logo" />
           </div>
           <button
-            className="absolute right-1 mr-2 md:hidden  text-white focus:outline-none"
+            className="absolute right-1 mr-2 md:hidden text-white focus:outline-none"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             <svg
@@ -53,43 +48,40 @@ const Navbar = () => {
           </button>
         </div>
 
-
-        <ul className={`lg:hidden md:hidden absolute top-16 gap-16 px-8 py-2 left-0 w-full bg-gradient-to-r from-yellow-500 via-brown-500 to-green-700 text-white md:static md:flex md:space-x-6 md:w-8  md:w-auto transition-all duration-900 ${isMenuOpen ? 'hidden' : 'block'} md:gap-6`}>
+        <ul className={`lg:hidden md:hidden absolute top-16 gap-16 px-8 py-2 left-0 w-full bg-gradient-to-r from-yellow-500 via-brown-500 to-green-700 text-white transition-all duration-900 ${isMenuOpen ? 'hidden' : 'block'} md:gap-6`}>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-            <a href="/products">Products</a>
+            <Link to="/products">Products</Link>
           </li>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-            <a href="/result">Our Result</a>
+            <Link to="/result">Our Result</Link>
           </li>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li className="hover:text-yellow-400 cursor-pointer p-4 md:p-0">
-            <a href="/contact">Contact Us</a>
+            <Link to="/contact">Contact Us</Link>
           </li>
         </ul>
 
-
         <div>
-          <ul className={`hidden md:flex gap-16 bg-gradient-to-r from-[#ada417] via-[#cfac1e] to-[#59902c]  px-8 py-2 left-0 w-full text-white`}>
+          <ul className="hidden md:flex gap-16 bg-gradient-to-r from-[#ada417] via-[#cfac1e] to-[#59902c] px-8 py-2 text-white">
             <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-              <a href="/">Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-              <a href="/products">Products</a>
+              <Link to="/products">Products</Link>
             </li>
             <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-            <a href="/results">Our Result</a>
-          </li>
-
-            <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-              <a href="/about">About</a>
+              <Link to="/results">Our Result</Link>
             </li>
             <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
-              <a href="/contact">Contact Us</a>
+              <Link to="/about">About</Link>
+            </li>
+            <li className="hover:text-gray-300 cursor-pointer p-4 md:p-0">
+              <Link to="/contact">Contact Us</Link>
             </li>
           </ul>
         </div>
@@ -97,23 +89,28 @@ const Navbar = () => {
         <div className='flex gap-6 text-white justify-center mr-6'>
           <div className="flex relative p-2 group">
             <VscAccount size={25} className='hover:text-gray-300' />
-            {/* Dropdown menu visible on hover */}
             <div className="absolute right-4 top-6 w-40 bg-gradient-to-r from-yellow-600 via-brown-500 to-green-600 text-white rounded-md hidden group-hover:block">
               <ul className="space-y-2 p-2">
-                <li className="hover:text-gray-200 border-b cursor-pointer"><a href='/Profile'>Profile</a></li>
-                <li className="hover:text-gray-200 my-4 cursor-pointer"><a href='/Order'>Orders</a></li>
+                <li className="hover:text-gray-200 border-b cursor-pointer">
+                  <Link to="/profile">Profile</Link>
+                </li>
+                <li className="hover:text-gray-200 my-4 cursor-pointer">
+                  <Link to="/order">Orders</Link>
+                </li>
               </ul>
             </div>
           </div>
-          <a className='mt-2' href='/Cart'><FaCartShopping size={25} className='hover:text-gray-300' /></a>
-          
-          
-          {!isLoggedIn &&
-              <button className='px-2 bg-orange-400 rounded-md'><a href="/Login" className=''>Login</a></button>
-            }
-          {isLoggedIn &&
+          <Link className='mt-2' to='/cart'>
+            <FaCartShopping size={25} className='hover:text-gray-300' />
+          </Link>
+          {!isLoggedIn && (
+            <button className='px-2 bg-orange-400 rounded-md'>
+              <Link to="/login">Login</Link>
+            </button>
+          )}
+          {isLoggedIn && (
             <button onClick={handleLogout} className='px-2 bg-red-400 rounded-md'>Logout</button>
-          }
+          )}
         </div>
       </div>
     </nav>
